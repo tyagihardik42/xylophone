5ebd38e9fb0d97e067e3eec1f13f0beae4a1f271
Hardik Tyagi <tyagi.hardik42@yahoo.com>
2020-09-08 18:01:01 +0530

    Change width of cells like a xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,22 @@ void main() {
 }
 
 class XyloPhoneApp extends StatelessWidget {
+
   void playSound(int soundNumber){
     final player = AudioCache();
     player.play('note$soundNumber.wav');
   }
-  Expanded buildKey({Color color, int soundNumber}){
+  Expanded buildKey({Color color, int soundNumber, double width}){
     return Expanded(
-      child: FlatButton(
-
-        onPressed: (){
-          playSound(soundNumber);
-        },
-        child: null,
-        color: color,
+      child: SizedBox(
+        width: width,
+        child: FlatButton(
+          onPressed: (){
+            playSound(soundNumber);
+          },
+          child: null,
+          color: color,
+        ),
       ),
     );
   }
@@ -31,7 +34,7 @@ class XyloPhoneApp extends StatelessWidget {
         backgroundColor: Colors.black,
         body: SafeArea(
           child:Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Padding(
                 padding: const EdgeInsets.all(20.0),
@@ -44,13 +47,13 @@ class XyloPhoneApp extends StatelessWidget {
                   ),
                 ),
               ),
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.blue, soundNumber: 5),
-              buildKey(color: Colors.indigo, soundNumber: 6),
-              buildKey(color: Colors.deepPurple, soundNumber: 7),
+              buildKey(color: Colors.red, soundNumber: 1,width: 380.0),
+              buildKey(color: Colors.orange, soundNumber: 2,width: 350.0),
+              buildKey(color: Colors.yellow, soundNumber: 3,width: 320.0),
+              buildKey(color: Colors.green, soundNumber: 4,width: 290.0),
+              buildKey(color: Colors.blue, soundNumber: 5,width: 260.0),
+              buildKey(color: Colors.indigo, soundNumber: 6,width: 230.0),
+              buildKey(color: Colors.deepPurple, soundNumber: 7,width: 200.0),
             ],
           ),
         ),
